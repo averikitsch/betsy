f879b784ccd5f69027044f3a1f1815b6e79809a0
enigmagnetic <lauren.cardella@gmail.com>
2017-10-17 16:34:30 -0700

    Add resources to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,87 +1,18 @@
 Rails.application.routes.draw do
-  get 'order_products/index'
 
-  get 'order_products/show'
+  resources :order_products
 
-  get 'order_products/new'
+  resources :products do
+    resources :reviews, only: [:new, :create]
+  end
 
-  get 'order_products/create'
+  resources :categories do
+    resources :products
+  end
 
-  get 'order_products/edit'
+  resources :users
 
-  get 'order_products/update'
-
-  get 'order_products/destroy'
-
-  get 'reviews/index'
-
-  get 'reviews/show'
-
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  get 'reviews/edit'
-
-  get 'reviews/update'
-
-  get 'reviews/destroy'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  get 'categories/index'
-
-  get 'categories/show'
-
-  get 'categories/new'
-
-  get 'categories/create'
-
-  get 'categories/edit'
-
-  get 'categories/update'
-
-  get 'categories/destroy'
-
-  get 'orders/index'
-
-  get 'orders/show'
-
-  get 'orders/new'
-
-  get 'orders/create'
-
-  get 'orders/edit'
-
-  get 'orders/update'
-
-  get 'orders/destroy'
-
-  get 'products/index'
-
-  get 'products/show'
-
-  get 'products/new'
-
-  get 'products/create'
-
-  get 'products/edit'
-
-  get 'products/update'
-
-  get 'products/destroy'
+  resources :orders
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
