51427831bf9f36301ef72cb2e859b6239b711dd7
enigmagnetic <lauren.cardella@gmail.com>
2017-10-17 17:02:58 -0700

    Add model relationships and categories join table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
-  belongs_to :category
-  belongs_to :merchant
+  has_and_belongs_to_many :categories
+  belongs_to :user
+  has_many :order_products
+  has_many :reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017231853) do
+ActiveRecord::Schema.define(version: 20171017235137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20171017231853) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_products", force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categories_products_on_category_id"
+    t.index ["product_id"], name: "index_categories_products_on_product_id"
   end
 
   create_table "order_products", force: :cascade do |t|
