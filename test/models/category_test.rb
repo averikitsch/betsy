a245d2920c81034103d44dd02c6f1f562edeb68d
averikitsch <aekitsch@gmail.com>
2017-10-18 12:43:22 -0700

    updated category model testing

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,29 +2,33 @@ require 'test_helper'
 
 describe Category do
   let(:category) { Category.new }
+  describe "validations" do
+    it "category without name is invalid" do
+      category.valid?.must_equal false
+      category.name = ''
+      category.valid?.must_equal false
+    end
 
-  it "category without name is invalid" do
-    category.valid?.must_equal false
-    category.name = ''
-    category.valid?.must_equal false
+    it "category with name is valid" do
+      category.name = 'decorations'
+      category.valid?.must_equal true
+    end
+
+    it "must be unique" do
+      category.name = 'decorations'
+      category.save
+      new_category = Category.new(name: 'decorations')
+      new_category.valid?.must_equal false
+    end
   end
+  describe "relations " do
+    it "has relations to product" do
+      prd = products(:one)
+      cat = Category.first
+      cat.must_respond_to :products
+      cat.products << prd
+      cat.products[0].must_equal products(:one)
+    end
 
-  it "category with name is valid" do
-    category.name = 'decorations'
-    category.valid?.must_equal true
-  end
-
-  it "must be unique" do
-    category.name = 'decorations'
-    category.save
-    new_category = Category.new(name: 'decorations')
-    new_category.valid?.must_equal false
-  end
-
-  it "has relations to product" do
-    prd = products(:one)
-    cat = Category.first
-    cat.products << prd
-    cat.products[0].must_equal products(:one)
   end
 end
