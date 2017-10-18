35bf1897c8da87ee94e341faae6b5ede5bf237fb
enigmagnetic <lauren.cardella@gmail.com>
2017-10-18 03:30:42 -0700

    Add order validations tests. All but two are passing--order products presence and default pending status.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,18 @@
 class Order < ApplicationRecord
   has_many :order_products
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :email, presence: true
+  validates :cc_num, numericality: { only_integer: true }, length: { is: 16 }
+  validates :cc_expiry, presence: true
+  validate :card_expiry_cannot_be_in_the_past
+  validates :cc_cvv, numericality: { only_integer: true }, length: { is: 3}
+  validates :billing_zip, numericality: { only_integer: true }
+
+  def card_expiry_cannot_be_in_the_past
+    if !cc_expiry.nil? && Date.strptime(cc_expiry, '%m/%y') < Date.today
+      errors.add(:cc_expiry, "can't be in the past")
+    end
+  end
 end
