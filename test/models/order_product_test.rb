ce50a8a916d8680e8cef1c38f1aed79e959c17f0
enigmagnetic <lauren.cardella@gmail.com>
2017-10-18 04:03:45 -0700

    Add order_product tests, but none are passing.

--- test/models/order_product_test.rb
+++ test/models/order_product_test.rb
@@ -1,9 +1,36 @@
 require "test_helper"
 
 describe OrderProduct do
-  let(:order_product) { OrderProduct.new }
+  let(:order_product) { OrderProduct.new(order_id: orders(:one), product_id: products(:one), quantity: 7) }
 
-  it "must be valid" do
-    value(order_product).must_be :valid?
+  describe "order product validations" do
+  it "must have a positive integer quantity" do
+    order_product.quantity = nil
+    order_product.valid?.must_equal false
+    order_product.quantity = "eight"
+    order_product.valid?.must_equal false
+    order_product.quantity = -3
+    order_product.valid?.must_equal false
+    order_product.quantity = 6
+    order_product.valid?.must_equal true
   end
+
+  it "must have a default shipped value of false" do
+    order_product.shipped.must_equal false
+  end
+
+  it "must have an associated order" do
+    order_product.order_id = nil
+    order_product.valid?.must_equal false
+    order_product.order_id = orders(:one).id
+    order_product.valid?.must_equal true
+  end
+
+  it "must have an associated product" do
+    order_product.product_id = nil
+    order_product.valid?.must_equal false
+    order_product.product_id = products(:one).id
+    order_product.valid?.must_equal true
+  end
+end
 end
