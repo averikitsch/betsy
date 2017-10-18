2956b0df2957e46107eb558299057f2d3aeac663
Laura Robertson <mslaura.mae@gmail.com>
2017-10-18 12:55:04 -0700

    Review model tests written

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -2,8 +2,19 @@ require "test_helper"
 
 describe Review do
   let(:review) { Review.new }
+  let(:one) { reviews(:one) }
+  let(:two) { products(:two) }
 
   it "must be valid" do
-    value(review).must_be :valid?
+    one.must_be :valid?
+  end
+
+  it "rating must be present" do
+    review.product_id = two.id
+    review.rating = "     "
+    review.valid?.must_equal false
+
+    review.rating = 2
+    review.valid?.must_equal true
   end
 end
