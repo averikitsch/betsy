51f0bff437ed2fbf5df9975bb94f49381f8e29e2
averikitsch <aekitsch@gmail.com>
2017-10-17 21:21:32 -0700

    tests for category model

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,30 @@
-require "test_helper"
+require 'test_helper'
 
 describe Category do
   let(:category) { Category.new }
 
-  it "must be valid" do
-    value(category).must_be :valid?
+  it "category without name is invalid" do
+    category.valid?.must_equal false
+    category.name = ''
+    category.valid?.must_equal false
+  end
+
+  it "category with name is valid" do
+    category.name = 'decorations'
+    category.valid?.must_equal true
+  end
+
+  it "must be unique" do
+    category.name = 'decorations'
+    category.save
+    new_category = Category.new(name: 'decorations')
+    new_category.valid?.must_equal false
+  end
+
+  it "has relations to product" do
+    prd = Product.first
+    cat = Category.first
+    prd << cat
+    cat.products[0] = products(:one)
   end
 end
