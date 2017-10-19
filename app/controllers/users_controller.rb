18c093bdc3f035aa5cbfcf11f3b679e73efcc05a
enigmagnetic <lauren.cardella@gmail.com>
2017-10-19 13:10:53 -0700

    Testing the app in the broswer, new user functionality is working, Tests are not passing.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    @auth_hash = request.env['omniauth.auth']
+    @user = User.find_by(uid: @auth_hash['uid'], provider: @auth_hash['provider'])
+
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome, #{@user.username}"
+    else
+      @user = User.new(uid: @auth_hash['uid'], provider: @auth_hash['provider'], username: @auth_hash['info']['nickname'], email: @auth_hash['info']['email'])
+
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:success] = "Welcome, #{@user.username}"
+      else
+        flash[:error] = "Unable to save user"
+      end
+    end
+    redirect_to root_path
   end
 
   def edit
@@ -25,7 +42,8 @@ class UsersController < ApplicationController
   end
 
   def login
+    user
   end
 
-  
+
 end
