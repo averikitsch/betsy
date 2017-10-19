fd8f796f2a5e0fb794bd1cccd760d9efac0c7aa9
Laura Robertson <mslaura.mae@gmail.com>
2017-10-18 20:11:35 -0700

    Deleted extra views for non exsitent routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,31 +3,18 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
-  def show
-  end
-
   def new
     @category = Category.new
   end
 
   def create
-    @category = Category.new
-
     @category = Category.new category_params
+
     if @category.save
       redirect_to categories_path
     else
       render :new
     end
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
   end
 
   private
