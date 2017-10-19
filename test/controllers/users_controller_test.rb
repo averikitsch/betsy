8f1ef906ee36ffdcb017e97364e36f4ab2c8778f
enigmagnetic <lauren.cardella@gmail.com>
2017-10-19 05:35:58 -0700

    Add users_controller tests for new user and login existing user. Edit users fixtures. Add OmniAuth test helper methods.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,37 +2,38 @@ require "test_helper"
 
 describe UsersController do
   it "should get index" do
-    get users_index_url
+    get users_path
     value(response).must_be :success?
   end
 
   it "should get show" do
-    get users_show_url
+    get user_path(users(:two))
     value(response).must_be :success?
   end
 
-  it "should get new" do
-    get users_new_url
-    value(response).must_be :success?
+  it "logs in an existing user" do
+    start_count = User.count
+    user = users(:one)
+
+    login(user, "github")
+    must_redirect_to root_path
+    session[:user_id].must_equal user.id
+    User.count.must_equal start_count
   end
 
-  it "should get create" do
-    get users_create_url
-    value(response).must_be :success?
-  end
+  it "creates a new user" do
+    start_count = User.count
+    user = User.new(provider: "github", uid: 99, username: "dot", email: "dot@user.com")
 
-  it "should get edit" do
-    get users_edit_url
-    value(response).must_be :success?
-  end
+    login(user, "github")
 
-  it "should get update" do
-    get users_update_url
-    value(response).must_be :success?
+    must_redirect_to root_path
+    User.count.must_equal start_count + 1
+    session[:user_id].must_equal User.last.id
   end
 
   it "should get destroy" do
-    get users_destroy_url
+    get user_path(users(:one))
     value(response).must_be :success?
   end
 
