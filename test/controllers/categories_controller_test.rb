80e6d4f60fde38fbe4df07499148541a12f9001e
Laura Robertson <mslaura.mae@gmail.com>
2017-10-20 12:55:36 -0700

    Baseline category routes and add a category tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,39 +1,30 @@
 require "test_helper"
 
 describe CategoriesController do
-  it "should get index" do
-    get categories_index_url
-    value(response).must_be :success?
+  let(:one) { categories(:one)}
+
+  describe "categories view routes" do
+    it "should get index" do
+      get categories_path
+      must_respond_with :success
+    end
+
+    it "should get new" do
+      get new_category_path
+      must_respond_with :success
+    end
   end
 
-  it "should get show" do
-    get categories_show_url
-    value(response).must_be :success?
-  end
+  describe "categories add new" do
+    it "a merchant can add a new category" do
+      proc { post categories_path, params: {category: {name: "Spooks"}}}.must_change 'Category.count', 1
+      must_respond_with :redirect
+    end
 
-  it "should get new" do
-    get categories_new_url
-    value(response).must_be :success?
-  end
-
-  it "should get create" do
-    get categories_create_url
-    value(response).must_be :success?
-  end
-
-  it "should get edit" do
-    get categories_edit_url
-    value(response).must_be :success?
-  end
-
-  it "should get update" do
-    get categories_update_url
-    value(response).must_be :success?
-  end
-
-  it "should get destroy" do
-    get categories_destroy_url
-    value(response).must_be :success?
+    it "must be a unique category" do
+      one.name.must_equal "potions"
+      proc { post categories_path, params: {category: {name: "potions"}}}.must_change 'Category.count', 0
+    end
   end
 
 end
