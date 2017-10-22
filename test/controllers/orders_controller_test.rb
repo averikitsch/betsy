f82c2a568c98738f4c14249412d19f4c8de69d55
Laura Robertson <mslaura.mae@gmail.com>
2017-10-22 15:13:04 -0700

    Order Index and destroy tests

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,39 +1,44 @@
 require "test_helper"
 
 describe OrdersController do
-  it "should get index" do
-    get orders_index_url
-    value(response).must_be :success?
+
+  describe "order index" do
+    it "should get index" do
+      get orders_path
+      must_respond_with :success
+    end
+
+    it "gets an order's products" do
+      post order_products_path(products(:three), order_product: {quantity: 1})
+      get orders_path
+      must_respond_with :success
+    end
   end
 
-  it "should get show" do
-    get orders_show_url
-    value(response).must_be :success?
-  end
+  # describe "order update" do
+  #   it "can update an order" do
+  #
+  #   end
+  #
+  #   it "reduces inventory for a product" do
+  #
+  #   end
+  # end
 
-  it "should get new" do
-    get orders_new_url
-    value(response).must_be :success?
-  end
+  describe "order destroy" do
+    it "can delete an order" do
+      proc {post order_products_path(products(:one), order_product: {quantity: 1})}.must_change 'Order.count', 1
+      Order.last.order_products.count.must_equal 1
+      proc {delete order_product_path(products(:one))}.must_change 'Order.count', -1
+    end
 
-  it "should get create" do
-    get orders_create_url
-    value(response).must_be :success?
-  end
+    it "removes session with deletion" do
+      post order_products_path(products(:one), order_product: {quantity: 1})
+      session[:order_id].must_equal Order.last.id
 
-  it "should get edit" do
-    get orders_edit_url
-    value(response).must_be :success?
-  end
-
-  it "should get update" do
-    get orders_update_url
-    value(response).must_be :success?
-  end
-
-  it "should get destroy" do
-    get orders_destroy_url
-    value(response).must_be :success?
+      delete order_product_path(products(:one))
+      # session[:order_id].must_equal nil
+    end
   end
 
 end
