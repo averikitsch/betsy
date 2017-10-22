7cca8e7f1e32f393f18601146ad28dadf3e2b94f
Laura Robertson <mslaura.mae@gmail.com>
2017-10-21 18:07:45 -0700

    Forms styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+// $(function(){ $(document).foundation(); });
 
 $(document).on('turbolinks:load', function() {
   $(function(){ $(document).foundation(); });
