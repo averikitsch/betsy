c97ef2e630d04dfbd9b7464ce891cfa8788fb70c
averikitsch <aekitsch@gmail.com>
2017-10-23 10:40:41 -0700

    order helper tests

--- test/helpers/orders_helper_test.rb
+++ test/helpers/orders_helper_test.rb
@@ -2,9 +2,10 @@ require "test_helper"
 
   describe OrdersHelper do
     #an order should have order_products
-
-
-    it "subtotals a list of items" do
-
+    it "test helper methods" do
+      order = orders(:one)
+      price = products(:one).price
+      subtotal(order.order_products).must_equal price
+      total(subtotal(order.order_products)).must_equal price*1.10
     end
   end
