54842d91e0fa9a95419e62fd82923f6d3af73c13
averikitsch <aekitsch@gmail.com>
2017-10-25 19:29:29 -0700

    updates to User index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :products
+  has_many :products, dependent: :destroy
+  
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 
