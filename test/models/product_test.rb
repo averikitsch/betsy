8ce2776ccee353d1a0f1f330e0d78328b85f1608
Laura Robertson <mslaura.mae@gmail.com>
2017-10-27 10:27:04 -0700

    Refactor model test: products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -10,54 +10,66 @@ describe Product do
     valid_product.valid?.must_equal false
     valid_product.errors.keys.must_include :user
   end
+
   it "must have a name to be valid" do
     one.valid?.must_equal true
     one.name = ""
     one.valid?.must_equal false
   end
+
   it "must have a unique name" do
     product = products(:two)
     product.name = products(:one).name
     product.valid?.must_equal false
   end
+
   it "must have a price" do
     product = products(:two)
     product.valid?.must_equal true
-    product.price = nil
-    product.valid?.must_equal false
-    product.price = "hello"
-    product.valid?.must_equal false
-    product.price = 0
-    product.valid?.must_equal false
+
+    [nil,"",0,"hello"].each do |num|
+      product.price = num
+      product.valid?.must_equal false
+    end
   end
+
   it "must have a numerical price value" do
+    product.user_id = two.id
     product.price = 10
     product.name = "unique name"
-    product.user_id = two.id
+
     product.stock = 5
     product.valid?.must_equal true
+
     product.price = "clearly not a number"
     product.valid?.must_equal false
   end
+
   it "must have a price value greater than 0" do
     product.user_id = two.id
     product.price = 10
     product.name = "unique name"
+
     product.stock = 1
     product.valid?.must_equal true
+
     product.price = -5
     product.valid?.must_equal false
   end
+
   it "has stock to be valid" do
     product = products(:two)
     product.valid?.must_equal true
+
+    [nil,"",-10].each do |num|
+      product.stock = num
+      product.valid?.must_equal false
+    end
+  end
+
+  it "can have 0 stock" do
+    product = products(:two)
     product.stock = 0
     product.valid?.must_equal true
-    product.stock = ""
-    product.valid?.must_equal false
-    product.stock = nil
-    product.valid?.must_equal false
-    product.stock = -10
-    product.valid?.must_equal false
   end
 end
