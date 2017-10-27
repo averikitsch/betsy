ddd16a8e0f3a3127b0dba5986d4ea90f77d0636b
enigmagnetic <lauren.cardella@gmail.com>
2017-10-26 19:59:45 -0700

    Use resources to consolidate necessary order_products routes. Remove redundant route. Put custom routes under standard resources for readability where appropriate.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
 
   root 'products#root'
 
-  # resources :order_products
-  get '/users/:user_id/products', to: 'products#index', as: 'user_products'
-  get '/users/:user_id/orders', to: 'users#order_fulfillment', as: 'user_orders'
-  get '/about', to: 'users#about'
-  get '/shipping', to: 'users#shipping'
   get '/orders/lookup', to: 'orders#lookup'
   get '/orders/found', to: 'orders#found'
 
@@ -21,18 +16,18 @@ Rails.application.routes.draw do
   resources :users do
     resources :products, only: [:index]
   end
+  get '/users/:user_id/orders', to: 'users#order_fulfillment', as: 'user_orders'
+  get '/about', to: 'users#about'
+  get '/shipping', to: 'users#shipping'
 
-  resources :orders
+  resources :orders, except: [:create, :edit]
 
+  resources :order_products, only: [:edit, :update, :destroy]
   patch '/order_products/:id/cancel', to: 'order_products#cancel', as: 'cancel_order_product'
   patch '/order_products/:id/shipped', to: 'order_products#shipped', as: 'ship_order_product'
   patch 'products/:id/toggle_active', to: 'products#toggle_active', as: 'toggle_active'
   post '/products/:id/order', to: 'order_products#create', as: 'order_products'
-  get '/order_products/:id/edit', to: 'order_products#edit', as: 'edit_order_product'
-  delete '/order_products/:id', to: 'order_products#destroy', as: 'order_product'
-  patch '/order_products/:id', to: 'order_products#update'
 
-  #get '/login', to: 'users#create'
 
   get '/auth/:provider/callback', to: 'users#login'
   delete '/logout', to: 'users#logout'
